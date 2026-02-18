7c123559c38431e1b4884eecc994ba9226e14c4b
Eloeunice <eloizaeunice25@gmail.com>
2026-02-17 21:30:38 -0400

    feat: add prisma

--- api/src/queue.ts
+++ api/src/queue.ts
@@ -1,20 +1,20 @@
-import { Queue, Worker, Job } from "bullmq";
+import { Queue } from "bullmq";
 import redis from "../../config/redisConfig";
 
-const emailQueue = new Queue("pdf", {
+const emailQueue = new Queue("email", {
     connection: redis,
 });
 
 // Define the job data structure
-interface PdfJobData {
+interface EmailJobData {
   to: string;
   subject: string;
   body: string;
 }
 
 // Add a job to the queue
- async function sendEmail(data: PdfJobData): Promise<string> {
-  const job = await emailQueue.add('send', data);
+ async function sendEmail(data: EmailJobData): Promise<string> {
+  const job = await emailQueue.add('send-email', data);
   return job.id!;
 }
 
--- api/src/server.ts
+++ api/src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { emailQueue, sendEmail} from "./queue";
+import prisma from "../../config/prisma.client";
 
 const app = express()
 app.use(express.json())
@@ -10,9 +11,19 @@ app.get("/send", (req, res) => {
 
 app.post("/send=email", async (req, res) => {
     const { to, subject, body } = req.body
+
+    const email = await prisma.email.create({
+        data: {
+            to,
+            subject,
+            body,
+            status: "PENDING"
+        }
+    })
     const jobId = await sendEmail({ to, subject, body })
     res.send({ message: "Email sent", jobId })
 })
+
 
 app.listen(3000, () => {
     console.log("Server running on port 3000")
